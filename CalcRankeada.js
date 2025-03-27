c9bb83dd4659bcce48da9e8dde3f4852499790dc
Arthur-Lukas <evangelistamachado01@gmail.com>
2025-03-27 19:03:34 -0300

    Desafio - 2

--- CalcRankeada.js
+++ CalcRankeada.js
@@ -1,24 +1,26 @@
-let vit = 90
-let der = 5
-
-function VitoOuDerro(resultado){
-    resultado = vit - der
-    
-    if (resultado<=10){
-        console.log("O jogador obteve um total de "+vit+" vitórias e um total de "+der+" derrotas, então o seu ranking é: "+(resultado="Ferro"))
-    }else if((resultado>10)&&(resultado<=20)){
-        console.log("O jogador obteve um total de "+vit+" vitórias e um total de "+der+" derrotas, então o seu ranking é: "+(resultado="Bronze"))
-    }else if((resultado>20)&&(resultado<=50)){
-        console.log("O jogador obteve um total de "+vit+" vitórias e um total de "+der+" derrotas, então o seu ranking é: "+(resultado="Prata"))
-    }else if((resultado>50)&&(resultado<=80)){
-        console.log("O jogador obteve um total de "+vit+" vitórias e um total de "+der+" derrotas, então o seu ranking é: "+(resultado="Ouro"))
-    }else if((resultado>80)&&(resultado<=90)){
-        console.log("O jogador obteve um total de "+vit+" vitórias e um total de "+der+" derrotas, então o seu ranking é: "+(resultado="Diamante"))
-    }else if((resultado>90)&&(resultado<=100)){
-        console.log("O jogador obteve um total de "+vit+" vitórias e um total de "+der+" derrotas, então o seu ranking é: "+(resultado="Lendário"))
-    }else{
-        console.log("O jogador obteve um total de "+vit+" vitórias e um total de "+der+" derrotas, então o seu ranking é: "+(resultado="Imortal"))
-    }
+function calcDoRanking(vit, der){
+    console.log("O herói tem um total de "+vit+" vitórias")
+    return vit-der
 }
 
-VitoOuDerro()
+let resultado = calcDoRanking(50 ,5)
+
+if(resultado<10){
+    resultado = "Ferro"
+}else if((resultado>10)&&(resultado<=20)){
+    resultado = "Bronze"
+}else if((resultado>20)&&(resultado<=50)){
+    resultado = "Prata"
+}else if((resultado>50)&&(resultado<=80)){
+    resultado = "Ouro"
+}else if((resultado>80)&&(resultado<=90)){
+    resultado = "Diamante"
+}else if((resultado>90)&&(resultado<=100)){
+    resultado = "Lendário"
+}else if(resultado>100){
+    resultado = "Imortal"
+}else{
+    resultado = "--Valor incorreto--"
+}
+
+console.log("Ele está no nível de "+resultado)
