e982774ab8195901ce5852282be515abc9d26536
Arthur-Lukas <evangelistamachado01@gmail.com>
2025-04-08 17:28:59 -0300

    Minhas alterações dentro do submódulo

--- 100ExercíciosGuanabara/16.js
+++ 100ExercíciosGuanabara/16.js
@@ -3,8 +3,8 @@ fumante. Pergunte a quantidade de cigarros fumados por dias e quantos anos ele
 já fumou. Considere que um fumante perde 10 min de vida a cada cigarro. Calcule
 quantos dias de vida um fumante perderá e exiba o total em dias.*/
 
-let cigarrosPorDia = 7
+let cigarros = 7
 let anosFumados = 2*365
-let diasPerdidos = cigarrosPorDia*anosFumados/144
+let diasPerdidos = cigarros*anosFumados/144
 
-console.log(Math.round(diasPerdidos))
+console.log("Vc fumou tanto que perdeu "+Math.round(diasPerdidos)+" dias de vida")
